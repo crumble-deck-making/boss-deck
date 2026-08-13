6ed8ec546da17228e9a9d14e1e243120f6d3dfee
crumble deck making <ivory14141@gmail.com>
2026-08-13 14:07:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,9 @@
-const CACHE = "deck-builder-v1";
-const FILES = ["./", "./index.html", "./manifest.webmanifest"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return r;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.claim())
   );
 });
+self.addEventListener('fetch',()=>{});
